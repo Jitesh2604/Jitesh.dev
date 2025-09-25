073ba41740a6b9977472a428d6290b8b3d0a614e
Jitesh <jiteshpal2604@gmail.com>
2025-09-25 17:27:56 +0530

    setup tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind is working 🚀
+      </h1>
+    </>
+  )
+}
+
+export default App
